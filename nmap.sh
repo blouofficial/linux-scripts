1bccdaae8dcadeac3b8de1547b2d37bd7aa69371
blouofficial <71730183+blouofficial@users.noreply.github.com>
2022-07-29 23:24:29 -0400

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -3,5 +3,5 @@
 read -p 'domain: ' domain
 read -p 'filename (nmap-<domain>.txt): ' file
 
-nmap -O -A $domain > $file
-nmap -sV --script vulners $domain >> $file
+sudo nmap -O -A $domain > $file
+sudo nmap -sV --script vulners $domain >> $file
